176422adce003c0169bae8cc972c15b0c13add27
Claudio Americo de Melo Filho <oclaudioamerico@gmail.com>
2020-03-29 09:30:12 -0300

    todas as rotas foram concluídas e testadas com o axios e o useHistory

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
 import api from "../../services/api";
@@ -14,6 +14,8 @@ export default function Register() {
 	const [city, setCity] = useState("");
 	const [uf, setUf] = useState("");
 
+	const history = useHistory();
+
 	async function handleRegister(e) {
 		e.preventDefault();
 		const data = {
@@ -26,6 +28,7 @@ export default function Register() {
 		try {
 			const response = await api.post("/ongs", data);
 			alert(`Seu ID de acesso ${response.data.id}`);
+			history.push("/");
 		} catch (error) {
 			alert(`Erro no cadastro, tente novamente.`);
 		}
